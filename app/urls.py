2880041226fe4dcf1f1b231e4479f54c7040e8f1
ㅤㅤㅤㅤ <zokirjonovbegzodbek93@gmail.com>
2024-11-14 22:25:28 +0500

    first commit

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,20 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import IndexApiView
+from .views import AuthorApi, ArticleApi, CommentApi, CategoryApi
 
 urlpatterns = [
-    path('', IndexApiView.as_view(), name='index'),
+    path('authors/', AuthorApi.as_view(), name='author-list'),
+    path('authors/<int:pk>/', AuthorApi.as_view(), name='author-detail'),
     
+    path('articles/', ArticleApi.as_view(), name='article-list'),
+    path('articles/<int:pk>/', ArticleApi.as_view(), name='article-detail'),
+    
+    path('comments/', CommentApi.as_view(), name='comment-list'),
+    path('comments/<int:pk>/', CommentApi.as_view(), name='comment-detail'),
+    
+    path('categories/', CategoryApi.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryApi.as_view(), name='category-detail'),
 ]
 
 if settings.DEBUG:
